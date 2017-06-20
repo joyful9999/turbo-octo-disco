efd5204b5439be3c0541c716c4d887562c066ee8
Joy Shih <joyful9999@gmail.com>
2017-06-20 16:05:35 +0800

    homework2

--- mAiLab_0002.py
+++ mAiLab_0002.py
@@ -1,5 +1,11 @@
+#mAiLab Lessen 2
+#2017-6-20
+#Joy Shih
 import numpy as np
 print (np.random.random_sample(5))
 rand= np.random.uniform(-1,1, size=100)
 print (np.mean(rand))
 print (np.std(rand))
+
+# answer of #1 [  7.24892526e-04   8.57738784e-01   8.60009426e-01   1.44072814e-02 8.73222769e-01]
+# answer of #2 average is 0.0265443074837 std = 0.569097933608
